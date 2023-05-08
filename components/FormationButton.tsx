d5ae390373392853063173f7e685f9946d5f4ab9
PolarOrb5705 <jedpatterson123@gmail.com>
2023-05-08 17:57:34 +0100

    Added Formation Picker
    Users can now manually set a formation

--- components/FormationButton.tsx
+++ components/FormationButton.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState } from "react";
+import { formationPositions } from "../data/formations";
 
 interface Props {
-  onFormationSelected: (formation: string) => void;
+  setFormation: (formation: string | null) => void;
 }
 
-const formations = ['4-3-3', '4-4-2', '3-5-2', '4-2-3-1', '4-1-4-1']; // Add more formations if you want
+const FormationButton: React.FC<Props> = ({ setFormation }) => {
+  const [manualFormation, setManualFormation] = useState("");
 
-const FormationButton: React.FC<Props> = ({ onFormationSelected }) => {
   const handleClick = () => {
-    const randomFormation = formations[Math.floor(Math.random() * formations.length)];
-    onFormationSelected(randomFormation);
+    const availableFormations = Object.keys(formationPositions);
+    const randomFormation =
+      availableFormations[
+        Math.floor(Math.random() * availableFormations.length)
+      ];
+    setFormation(randomFormation);
+    setManualFormation(randomFormation);
+  };
+
+  const handleManualFormationChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setManualFormation(event.target.value);
+    setFormation(event.target.value);
   };
 
   return (
-    <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      onClick={handleClick}
-    >
-      Get Random Formation
-    </button>
+    <div className="flex flex-col items-center mb-8">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+        onClick={handleClick}
+      >
+        Get Random Formation
+      </button>
+      <label htmlFor="manualFormation" className="mb-2">
+        Or choose a formation:
+      </label>
+      <div className="flex items-center">
+        <select
+          id="manualFormation"
+          value={manualFormation}
+          onChange={handleManualFormationChange}
+          className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+        >
+          <option value="">Select a formation</option>
+          {Object.keys(formationPositions).map((formation) => (
+            <option key={formation} value={formation}>
+              {formation}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
   );
 };
 
