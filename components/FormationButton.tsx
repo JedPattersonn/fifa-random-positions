be274628f45a3247d3f34f27284e434273bf2c3d
PolarOrb5705 <jedpatterson123@gmail.com>
2023-05-08 18:01:39 +0100

    Update FormationButton.tsx
    Center formation text inside the picker

--- components/FormationButton.tsx
+++ components/FormationButton.tsx
@@ -41,7 +41,7 @@ const FormationButton: React.FC<Props> = ({ setFormation }) => {
           id="manualFormation"
           value={manualFormation}
           onChange={handleManualFormationChange}
-          className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+          className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline text-center"
         >
           <option value="">Select a formation</option>
           {Object.keys(formationPositions).map((formation) => (
