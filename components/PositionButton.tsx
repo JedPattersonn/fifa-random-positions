4e81e8c9d83936052c660c9d256ce4dd88bb74d4
PolarOrb5705 <jedpatterson123@gmail.com>
2023-05-08 15:11:53 +0100

    Fix React

--- components/PositionButton.tsx
+++ components/PositionButton.tsx
@@ -49,7 +49,7 @@ const PositionButton: React.FC<Props> = ({ formation }) => {
           onChange={(e) => setIncludeAny(e.target.checked)}
           className="mr-2"
         />
-        Include 'Any' position
+        Include Any position
       </label>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
