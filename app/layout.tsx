47a947b645f1ddf683f8d889f915c9555728a5a3
Jed Patterson <jedpatterson123@gmail.com>
2023-10-13 00:47:10 +0100

    SEO changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,30 @@ export const metadata: Metadata = {
   title: "Random Fifa Positions - EA FC 24",
   description:
     "Get random fifa formations and positions for you and your friends. Up to date for EA FC 24. Great for pro clubs, friendlies, and ultimate team.",
+  alternates: {
+    canonical: "https://randomfifapositions.com",
+  },
+  creator: "Jed Patterson",
+
+  keywords: [
+    "fifa",
+    "random",
+    "fifa 24",
+    "fifa 23",
+    "ea fc",
+    "ea fc 24",
+    "positions",
+    "pro clubs",
+    "ultimate team",
+    "random fifa positions",
+    "random fifa",
+    "random fifa formation",
+    "random fifa 24",
+    "random fifa 23",
+    "random ea fc",
+    "random ea fc 24",
+    "random positions",
+  ],
 };
 
 export default function RootLayout({
