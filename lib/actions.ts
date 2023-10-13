12eb5588da114282b8a439fa2813731cc59aa52b
Jed Patterson <jedpatterson123@gmail.com>
2023-10-13 20:21:54 +0100

    Fix type

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,9 +1,12 @@
 "use server";
 import { supabase } from "./supabase";
+import { Database } from "@/database.types";
 
 export default async function NewFormation(formData: FormData) {
   const { data, error } = await supabase.from("analytics").insert({
-    formation: formData.get("formation") as string,
+    formation: formData.get(
+      "formation"
+    ) as Database["public"]["Enums"]["formation_name"],
   });
 
   if (error) {
