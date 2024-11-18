594159bf09e0ddc25155dd61cc4904ce2a9ee49e
Jed Patterson <jedpatterson123@gmail.com>
2024-11-18 11:04:40 +0000

    Add adsense script tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import FormationButton from "@/Components/FormationButton";
 import PositionButton from "@/Components/PositionButton";
+import Script from "next/script";
 import Footer from "@/Components/Footer";
 import { useState } from "react";
 
@@ -9,6 +10,11 @@ export default function Home() {
 
   return (
     <div className="container mx-auto flex flex-col items-center justify-center min-h-screen py-2">
+      <Script
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7629352561801539"
+        async
+        crossOrigin="anonymous"
+      />
       <h1 className="text-4xl mb-6">Fifa Position Generator</h1>
       <FormationButton setFormation={setFormation} />
 
